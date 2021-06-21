88aa99a96e75a358f3759240259d2645fa3b3c66
Rawan19 <moonlight142011@gmail.com>
2021-06-22 00:00:36 +0200

    main_file

---
+++ pipeline.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import Model
+import encode
+
+from sklearn.svm import SVC
+
+def fill_nulls(dataframe):
+        pass
+
+
+def one_hot_encode(df):
+	pass
+
+
+
+def Model(x, y, a):
+	
+    pass
+
+
+
