3ffb77ea3304eb0d5cb2b9ebb024de5c57163f8f
Rawan19 <moonlight142011@gmail.com>
2021-06-22 00:33:50 +0200

    modeling with logitic regression

--- pipeline.py
+++ pipeline.py
@@ -3,6 +3,7 @@ from sklearn.preprocessing import StandardScaler
 import Model
 import encode
 
+from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 
 def fill_nulls(dataframe):
@@ -14,9 +15,20 @@ def one_hot_encode(df):
 
 
 
-def Model(x, y, a):
+def Model(train_df, test_df):
 	
-    pass
+    
+
+        X_train = train_df.drop("Survived", axis=1)
+        Y_train = train_df["Survived"]
+        X_test  = test_df.drop("PassengerId", axis=1).copy()
+        X_train.shape, Y_train.shape, X_test.shape
+
+        logreg = LogisticRegression()
+        logreg.fit(X_train, Y_train)
+        Y_pred = logreg.predict(X_test)
+        acc_log = round(logreg.score(X_train, Y_train) * 100, 2)
+        acc_log
 
 
 
